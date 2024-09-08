a59116c88a46bd18041da2998230e2574bd73a3e
Riku Brandt <brandtriku@gmail.com>
2024-09-09 00:13:06 +0300

    Dont show stuff that hasnt been processed by cloud funcs

--- components/GridSlider.js
+++ components/GridSlider.js
@@ -11,9 +11,12 @@ const GridSlider = ({ post }) => {
   const totalSlides = post.length;
   const [touchStart, setTouchStart] = useState(null);
   const [touchEnd, setTouchEnd] = useState(null);
+  const [allProcessed, setAllProcessed] = useState(false);
 
   useEffect(() => {
-    sliderRef.current.style.transform = `translateX(-${currentSlide * 100}%)`;
+    if (sliderRef.current) {
+      sliderRef.current.style.transform = `translateX(-${currentSlide * 100}%)`;
+    }
   }, [currentSlide]);
 
   // Function to determine if the file is an image or video using Firebase metadata
@@ -23,7 +26,9 @@ const GridSlider = ({ post }) => {
     try {
       const metadata = await getMetadata(fileRef);
       const contentType = metadata.contentType;
-
+      if (metadata.customMetadata && metadata.customMetadata.processed !== "true") {
+        return "processing"; // File is still processing
+      }
       if (contentType.startsWith("image/")) {
         return "image";
       } else if (contentType.startsWith("video/")) {
@@ -47,6 +52,9 @@ const GridSlider = ({ post }) => {
         })
       );
       setFileTypes(types);
+      // Check if all files are processed
+      const allProcessed = types.every((type) => type !== "processing");
+      setAllProcessed(allProcessed);
     };
 
     fetchFileTypes();
@@ -95,7 +103,13 @@ const GridSlider = ({ post }) => {
     setTouchStart(null);
     setTouchEnd(null);
   };
-
+  if (!allProcessed) {
+    return (
+      <div className="w-full h-full flex items-center justify-center">
+        <p>Processing media, please wait...</p>
+      </div>
+    );
+  }
   return (
     <div className="relative w-full h-full overflow-hidden">
       {/* Carousel wrapper */}
@@ -118,6 +132,7 @@ const GridSlider = ({ post }) => {
                   playing={isPlaying && currentSlide === index} 
                   muted={isMuted} 
                   playsinline={true}
+                  loop={true}
                   className="block w-full h-full object-contain"
                   controls={false} 
                   width="100%"
@@ -129,7 +144,7 @@ const GridSlider = ({ post }) => {
                     className="absolute inset-0 flex items-center justify-center cursor-pointer"
                     onClick={handlePlayPause} // Play video on click
                   >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                 <path fillRule="evenodd" d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z" clip-rule="evenodd" />
                 </svg>
 
@@ -143,12 +158,12 @@ const GridSlider = ({ post }) => {
                     onClick={handleMuteToggle}
                   >
                     {isMuted ? (
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                         <path d="M13.5 4.06c0-1.336-1.616-2.005-2.56-1.06l-4.5 4.5H4.508c-1.141 0-2.318.664-2.66 1.905A9.76 9.76 0 0 0 1.5 12c0 .898.121 1.768.35 2.595.341 1.24 1.518 1.905 2.659 1.905h1.93l4.5 4.5c.945.945 2.561.276 2.561-1.06V4.06ZM17.78 9.22a.75.75 0 1 0-1.06 1.06L18.44 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06l1.72-1.72 1.72 1.72a.75.75 0 1 0 1.06-1.06L20.56 12l1.72-1.72a.75.75 0 1 0-1.06-1.06l-1.72 1.72-1.72-1.72Z" />
                       </svg>
                       
                     ) : (
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                     <path d="M13.5 4.06c0-1.336-1.616-2.005-2.56-1.06l-4.5 4.5H4.508c-1.141 0-2.318.664-2.66 1.905A9.76 9.76 0 0 0 1.5 12c0 .898.121 1.768.35 2.595.341 1.24 1.518 1.905 2.659 1.905h1.93l4.5 4.5c.945.945 2.561.276 2.561-1.06V4.06ZM18.584 5.106a.75.75 0 0 1 1.06 0c3.808 3.807 3.808 9.98 0 13.788a.75.75 0 0 1-1.06-1.06 8.25 8.25 0 0 0 0-11.668.75.75 0 0 1 0-1.06Z" />
                     <path d="M15.932 7.757a.75.75 0 0 1 1.061 0 6 6 0 0 1 0 8.486.75.75 0 0 1-1.06-1.061 4.5 4.5 0 0 0 0-6.364.75.75 0 0 1 0-1.06Z" />
                     </svg>
